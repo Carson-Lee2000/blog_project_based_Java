831c5d8cb2131e8b9ad97c66fea94a5f6a1f2013
Carson-Lee2000 <1572403423@qq.com>
2022-08-14 21:42:26 +0800

    初始化工程提交

---
+++ src/main/java/com/lks/blog/blog_project/controller/AlphaController.java
@@ -0,0 +1,16 @@
+package com.lks.blog.blog_project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/alpha")
+public class AlphaController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello spring boot";
+    }
+}
