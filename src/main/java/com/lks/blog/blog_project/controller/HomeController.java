ea7408cf6a8b5f073a95fcde0ad0b085d07689fd
Carson-Lee2000 <1572403423@qq.com>
2022-09-10 18:31:33 +0800

    设置分页，设置日志系统

---
+++ src/main/java/com/lks/blog/blog_project/controller/HomeController.java
@@ -0,0 +1,51 @@
+package com.lks.blog.blog_project.controller;
+
+import com.lks.blog.blog_project.entity.DiscussPost;
+import com.lks.blog.blog_project.entity.Page;
+import com.lks.blog.blog_project.entity.User;
+import com.lks.blog.blog_project.service.DiscussPostService;
+import com.lks.blog.blog_project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class HomeController {
+
+    private final DiscussPostService discussPostService;
+    private final UserService userService;
+
+    @Autowired
+    public HomeController(DiscussPostService discussPostService, UserService userService) {
+        this.discussPostService = discussPostService;
+        this.userService = userService;
+    }
+
+    @RequestMapping(path = "/index", method = RequestMethod.GET)
+    public String getIndexPage(Model model, Page page) {
+        page.setRows(discussPostService.findDiscussPostRows(0));
+        page.setPath("/index");
+
+        List<DiscussPost> list = discussPostService.findDiscussPosts(0, page.getOffset(), page.getLimit());
+        List<Map<String, Object>> discussPosts = new ArrayList<>();
+        if (list != null) {
+            for (DiscussPost post : list) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("post", post);
+                User user = userService.findUserById(post.getUserId());
+                map.put("user", user);
+                discussPosts.add(map);
+            }
+        }
+
+        model.addAttribute("discussPosts", discussPosts);
+        return "/index";
+    }
+}
--- src/main/java/com/lks/blog/blog_project/service/UserService.java
+++ src/main/java/com/lks/blog/blog_project/service/UserService.java
@@ -1,8 +1,22 @@
 package com.lks.blog.blog_project.service;
 
+import com.lks.blog.blog_project.dao.UserMapper;
+import com.lks.blog.blog_project.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private final UserMapper userMapper;
+
+    @Autowired
+    public UserService(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
+    public User findUserById(int id) {
+        return userMapper.selectById(id);
+    }
 
 }
