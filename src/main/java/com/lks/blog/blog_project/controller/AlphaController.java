8258c24ab25edb45d95490f72f3b380836e67d77
Carson-Lee2000 <1572403423@qq.com>
2022-09-11 19:27:08 +0800

    动态生成验证码

--- src/main/java/com/lks/blog/blog_project/controller/AlphaController.java
+++ src/main/java/com/lks/blog/blog_project/controller/AlphaController.java
@@ -1,8 +1,13 @@
 package com.lks.blog.blog_project.controller;
 
+import com.lks.blog.blog_project.util.CommunityUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/alpha")
@@ -12,5 +17,20 @@ public class AlphaController {
     @ResponseBody
     public String sayHello() {
         return "Hello spring boot";
+    }
+
+
+    // cookie demo
+    @RequestMapping(path = "/cookie/set", method = RequestMethod.GET)
+    @ResponseBody
+    public String setCookie(HttpServletResponse response) {
+        Cookie cookie = new Cookie("code", CommunityUtil.generateUUID());
+        // 设置cookie的生效范围
+        cookie.setPath("/community/alpha");
+        // 设置cookie的生存时间s
+        cookie.setMaxAge(60 * 10);
+        // 发送cookie给浏览器
+        response.addCookie(cookie);
+        return "set cookie";
     }
 }
